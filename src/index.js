5786a35dc89c7722337e3a0f0c24433ea1113067
Andrew J <aandjjam@gmail.com>
2015-08-23 15:20:58 +0100

    scroll aniamtion

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import * as Router from 'react-router';
-import { run, DefaultRoute, Route, RouteHandler } from "react-router";
+import { run, DefaultRoute, Route, RouteHandler } from 'react-router';
+//import tweenState from 'react-tween-state';
+import { Tween, Easing, update} from "tween.js";
+//import * as TWEEN from "tween.js"
 
 import MessageBox from './components/messagebox';
 
 require("bootstrap-webpack");
 require("./static/css/styles.css");
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+
+//class App extends React.Component {
+const App = React.createClass({
+  //mixins: [tweenState.Mixin],
+  scrollTop: 0,
+  getInitialState: function() {
+    return {
       feed: [
         {who: 0, message: "Yo!, You there?"},
         {who: 1, message: "Yeah, sup?"},
@@ -20,21 +26,22 @@ class App extends React.Component {
         {who: 0, message: "No worries, next time."},
         {who: 1, message: "fo sho"},
       ]
-    }
-  }
-  //<div className="msg">
-  //  <div className="bubble">
-  //    <p>Yo! You there?</p>
-  //  </div>
-  //</div>
-  //<div className="msg msg-out">
-  //  <div className="bubble">
-  //    <p>Yeah, sup?</p>
-  //  </div>
-  //</div>
+    };
+  },
   handleMessage(value) {
+    const node = this.refs.convo.getDOMNode();
     this.setState({feed: this.state.feed.concat([{who: 1, message: value}])})
-  }
+    const ns = node.scrollHeight - node.offsetHeight;
+    setInterval(() => {update(Date().now)}, 40)
+    new Tween({x: this.scrollTop})
+     .to({x: ns + 20}, 250)
+     .easing(Easing.Quadratic.Out)
+     .onUpdate(function() {
+     node.scrollTop = this.x;
+     })
+     .start();
+    this.scrollTop = ns;
+  },
   render() {
     return (
       <div className="container-fluid">
@@ -42,16 +49,17 @@ class App extends React.Component {
           <h1 className="title">Messages</h1>
         </header>
         <div className="content content-padded">
-          <div className="convo"> {
-           this.state.feed.map((m) => {
-             return (
-               <div className={"msg" + (m.who === 1? " msg-out" : "")}>
-                 <div className="bubble">
-                   <p>{m.message}</p>
+          <div ref="convo" className="convo"> {
+           this.state.feed
+             .map(m => {
+               return (
+                 <div className={"msg" + (m.who === 1? " msg-out" : "")}>
+                   <div className="bubble">
+                     <p>{m.message}</p>
+                   </div>
                  </div>
-               </div>
-             )
-           })
+               )
+             })
           } </div>
           <footer>
             <hr/>
@@ -63,7 +71,7 @@ class App extends React.Component {
       </div>
     );
   }
-}
+});
 
 //<Route name="home" handler={Home}/>
 //<DefaultRoute>
