e101e4895215ac12f723b0a8ae233f1bbb4b3703
faheem <faheem@faheempatel.com>
2015-08-26 20:25:41 +0100

    Add cssnext dependency

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,25 @@ var webpack = require('webpack');
 
 module.exports = {
   entry:[
-    "webpack-dev-server/client?http://0.0.0.0:8080",
-    "webpack/hot/only-dev-server",
-    "./src/index.js"
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js',
     ],
   output: {
     path: __dirname,
-    filename: "bundle.js"
+    filename: 'bundle.js',
+  },
+  cssnext: {
+    browsers: 'last 2 versions',
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot", "babel-loader"]},
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-    ]
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader'] },
+      { test: /\.css$/, loader: 'style-loader!css-loader!cssnext-loader' },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+    new webpack.NoErrorsPlugin(),
+  ],
 };
